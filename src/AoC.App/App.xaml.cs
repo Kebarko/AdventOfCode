e31be87e24bb27fd0c062a4cb60bc41f021205ef
Miroslav Rábek <rabek.miroslav@gmail.com>
2026-06-21 15:33:20 +0200

    Add project files.

---
+++ src/AoC.App/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using KE.AoC.App.Views;
+using System.Windows;
+
+namespace KE.AoC.App;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var window = new MainWindow();
+        window.Show();
+    }
+}
---
+++ src/AoC.App/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace KE.AoC.App.Views;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
