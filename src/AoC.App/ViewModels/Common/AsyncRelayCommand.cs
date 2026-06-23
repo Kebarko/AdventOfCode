63bf73a20d47ea2e24189a4397d149054ed3c58b
Miroslav Rábek <rabek.miroslav@gmail.com>
2026-06-23 15:54:00 +0200

    Add AsyncRelayCommand and ViewModelBase for MVVM support.

---
+++ src/AoC.App/ViewModels/Common/AsyncRelayCommand.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows.Input;
+
+namespace KE.AoC.App.ViewModels.Common;
+
+/// <summary>
+/// Represents an asynchronous command that can be bound to UI elements in a WPF application.
+/// </summary>
+internal class AsyncRelayCommand(Func<Task> execute, Func<bool>? canExecute = null) : ICommand
+{
+    private bool isExecuting;
+
+    /// <summary>
+    /// Event that is raised when the ability of the command to execute changes.
+    /// </summary>
+    public event EventHandler? CanExecuteChanged;
+
+    /// <summary>
+    /// Determines whether the command can execute in its current state.
+    /// </summary>
+    /// <param name="parameter">The parameter passed to the command.</param>
+    /// <returns>true if the command can execute; otherwise, false.</returns>
+    public bool CanExecute(object? parameter)
+    {
+        return !isExecuting && (canExecute?.Invoke() ?? true);
+    }
+
+    /// <summary>
+    /// Executes the command asynchronously.
+    /// </summary>
+    /// <param name="parameter">The parameter passed to the command.</param>
+    public async void Execute(object? parameter)
+    {
+        if (!CanExecute(parameter))
+            return;
+
+        isExecuting = true;
+        RaiseCanExecuteChanged();
+
+        try
+        {
+            await execute();
+        }
+        finally
+        {
+            isExecuting = false;
+            RaiseCanExecuteChanged();
+        }
+    }
+
+    /// <summary>
+    /// Raises the CanExecuteChanged event to notify that the command's ability to execute has changed.
+    /// </summary>
+    public void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
---
+++ src/AoC.App/ViewModels/Common/ViewModelBase.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace KE.AoC.App.ViewModels.Common;
+
+/// <summary>
+/// Base class for view models that implements INotifyPropertyChanged to support data binding in MVVM architecture.
+/// </summary>
+public abstract class ViewModelBase : INotifyPropertyChanged
+{
+    /// <summary>
+    /// Event that is raised when a property value changes. This event is part of the INotifyPropertyChanged interface and is used to notify the UI of changes in the view model's properties.
+    /// </summary>
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /// <summary>
+    /// Raises the PropertyChanged event for the specified property name. This method is called whenever a property value changes, allowing the UI to update accordingly.
+    /// </summary>
+    /// <param name="propertyName">The name of the property that changed.</param>
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    /// <summary>
+    /// Sets the value of a property and raises the PropertyChanged event if the value has changed. This method is used to simplify property setters in view models, ensuring that the UI is notified of changes.
+    /// </summary>
+    /// <typeparam name="T">The type of the property.</typeparam>
+    /// <param name="field">The backing field for the property.</param>
+    /// <param name="value">The new value for the property.</param>
+    /// <param name="propertyName">The name of the property that changed.</param>
+    /// <returns>true if the property value changed; otherwise, false.</returns>
+    protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value))
+            return false;
+
+        field = value;
+        OnPropertyChanged(propertyName);
+
+        return true;
+    }
+}
