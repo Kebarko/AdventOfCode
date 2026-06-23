802b9b6e9cf07417f0fac5726c4c4f87ce79998d
Miroslav Rábek <rabek.miroslav@gmail.com>
2026-06-23 15:26:28 +0200

    Add SolutionsAssemblyMarker class as assembly identifier

---
+++ src/AoC.Solutions/SolutionsAssemblyMarker.cs
@@ -0,0 +1,6 @@
+﻿namespace KE.AoC.Solutions;
+
+/// <summary>
+/// This class is used as a marker to identify the assembly containing the solutions for Advent of Code.
+/// </summary>
+public sealed class SolutionsAssemblyMarker { }
