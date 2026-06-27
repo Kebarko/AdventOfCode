c7f5ceacb964aa589cdbdfcbc7bccf50839313c3
Miroslav Rábek <rabek.miroslav@gmail.com>
2026-06-27 17:55:57 +0200

    Add generic MathUtils.Digits<T> method for digit counting.

---
+++ src/AoC.Solutions/Common/MathUtils.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace KE.AoC.Solutions.Common
+{
+    /// <summary>
+    /// Provides common mathematical utility methods.
+    /// </summary>
+    internal static class MathUtils
+    {
+        /// <summary>
+        /// Calculates the number of digits in the given value.
+        /// </summary>
+        /// <typeparam name="T">The type of the value.</typeparam>
+        /// <param name="value">The value to calculate the number of digits for.</param>
+        /// <returns>The number of digits in the value.</returns>
+        public static int Digits<T>(T value) where T : IBinaryInteger<T>
+        {
+            T ten = T.CreateChecked(10);
+
+            int count = 0;
+            do
+            {
+                value /= ten;
+                count++;
+            }
+            while (value != T.Zero);
+
+            return count;
+        }
+    }
+}
