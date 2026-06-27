52d0dcaf7757d3a8715b224362fd03eddfc7e375
Miroslav Rábek <rabek.miroslav@gmail.com>
2026-06-27 12:23:36 +0200

    Add clickable link to display a puzzle on AoC website.

---
+++ src/AoC.App/ViewModels/Common/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Input;
+
+namespace KE.AoC.App.ViewModels.Common;
+
+/// <summary>
+/// Represents an synchronous command that can be bound to UI elements in a WPF application.
+/// </summary>
+public class RelayCommand(Action execute, Func<bool>? canExecute = null) : ICommand
+{
+    /// <summary>
+    /// Event that is raised when the ability of the command to execute changes.
+    /// </summary>
+    public event EventHandler? CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    /// <summary>
+    /// Determines whether the command can execute in its current state.
+    /// </summary>
+    /// <param name="parameter">The parameter passed to the command.</param>
+    /// <returns>true if the command can execute; otherwise, false.</returns>
+    public bool CanExecute(object? parameter)
+    {
+        return canExecute?.Invoke() ?? true;
+    }
+
+    /// <summary>
+    /// Executes the command synchronously.
+    /// </summary>
+    /// <param name="parameter">The parameter passed to the command.</param>
+    public void Execute(object? parameter)
+    {
+        execute();
+    }
+}
--- src/AoC.App/ViewModels/DayViewModel.cs
+++ src/AoC.App/ViewModels/DayViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using KE.AoC.App.ViewModels.Common;
 using KE.AoC.Core.Input;
 using KE.AoC.Core.Solution;
+using System.Diagnostics;
 
 namespace KE.AoC.App.ViewModels;
 
@@ -25,6 +26,16 @@ public sealed class DayViewModel : ViewModelBase
     public PartViewModel? Part2 { get; }
 
     /// <summary>
+    /// Gets the URL for the specified day on the Advent of Code website.
+    /// </summary>
+    public Uri Url { get; }
+
+    /// <summary>
+    /// Gets the command to open the puzzle for the specified day in the default web browser.
+    /// </summary>
+    public RelayCommand OpenPuzzleCommand { get; }
+
+    /// <summary>
     /// Initializes a new instance of the <see cref="DayViewModel"/> class with the specified solution descriptor, input provider, and solution runner.
     /// </summary>
     /// <param name="descriptor">The solution descriptor for the specified day.</param>
@@ -36,5 +47,19 @@ public sealed class DayViewModel : ViewModelBase
         Part1 = new PartViewModel(descriptor, inputProvider, runner, 1);
         if (descriptor.HasPartTwo)
             Part2 = (PartViewModel?)new PartViewModel(descriptor, inputProvider, runner, 2);
+        Url = new($"https://adventofcode.com/{descriptor.Year}/day/{Day}");
+        OpenPuzzleCommand = new RelayCommand(OpenPuzzle);
+    }
+
+    private void OpenPuzzle()
+    {
+        try
+        {
+            Process.Start(new ProcessStartInfo(Url.AbsoluteUri) { UseShellExecute = true });
+        }
+        catch
+        {
+            Debug.WriteLine($"Failed to open URL: {Url.AbsoluteUri}");
+        }
     }
 }
