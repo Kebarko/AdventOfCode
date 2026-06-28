157d0d50844c60b369c459103cdde58aaaf3505f
Miroslav Rábek <rabek.miroslav@gmail.com>
2026-06-28 12:38:17 +0200

    Convert to file-scoped namespaces.

--- src/AoC.Solutions/Common/MathUtils.cs
+++ src/AoC.Solutions/Common/MathUtils.cs
@@ -1,31 +1,30 @@
 ﻿using System.Numerics;
 
-namespace KE.AoC.Solutions.Common
+namespace KE.AoC.Solutions.Common;
+
+/// <summary>
+/// Provides common mathematical utility methods.
+/// </summary>
+internal static class MathUtils
 {
     /// <summary>
-    /// Provides common mathematical utility methods.
+    /// Calculates the number of digits in the given value.
     /// </summary>
-    internal static class MathUtils
+    /// <typeparam name="T">The type of the value.</typeparam>
+    /// <param name="value">The value to calculate the number of digits for.</param>
+    /// <returns>The number of digits in the value.</returns>
+    public static int Digits<T>(T value) where T : IBinaryInteger<T>
     {
-        /// <summary>
-        /// Calculates the number of digits in the given value.
-        /// </summary>
-        /// <typeparam name="T">The type of the value.</typeparam>
-        /// <param name="value">The value to calculate the number of digits for.</param>
-        /// <returns>The number of digits in the value.</returns>
-        public static int Digits<T>(T value) where T : IBinaryInteger<T>
+        T ten = T.CreateChecked(10);
+
+        int count = 0;
+        do
         {
-            T ten = T.CreateChecked(10);
-
-            int count = 0;
-            do
-            {
-                value /= ten;
-                count++;
-            }
-            while (value != T.Zero);
-
-            return count;
+            value /= ten;
+            count++;
         }
+        while (value != T.Zero);
+
+        return count;
     }
 }
