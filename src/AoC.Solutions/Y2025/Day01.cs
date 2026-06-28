5d4bdfebb46b3d5ed7a66aed3bc3a2ca837f88a2
Miroslav Rábek <rabek.miroslav@gmail.com>
2026-06-28 12:25:07 +0200

    Convert to file-scoped namespaces.

--- src/AoC.Solutions/Y2025/Day01.cs
+++ src/AoC.Solutions/Y2025/Day01.cs
@@ -1,89 +1,88 @@
 ﻿using KE.AoC.Core.Solution;
 
-namespace KE.AoC.Solutions.Y2025
+namespace KE.AoC.Solutions.Y2025;
+
+[Solution(2025, 1)]
+public sealed class Day01 : SolutionBase
 {
-    [Solution(2025, 1)]
-    public sealed class Day01 : SolutionBase
+    /// <summary>
+    /// Calculates the number of times the pointer lands on zero after performing a series of rotations.
+    /// </summary>
+    public override object PartOne(string input)
     {
-        /// <summary>
-        /// Calculates the number of times the pointer lands on zero after performing a series of rotations.
-        /// </summary>
-        public override object PartOne(string input)
+        string[] rotations = Lines(input);
+
+        const int size = 100;
+        int current = 50;
+        int zeros = 0;
+        foreach (string rotation in rotations)
         {
-            string[] rotations = Lines(input);
+            char direction = rotation[0];
+            int distance = int.Parse(rotation[1..]);
 
-            const int size = 100;
-            int current = 50;
-            int zeros = 0;
-            foreach (string rotation in rotations)
+            switch (direction)
             {
-                char direction = rotation[0];
-                int distance = int.Parse(rotation[1..]);
+                case 'R':
+                    current = (current + distance) % size;
+                    break;
 
-                switch (direction)
-                {
-                    case 'R':
+                case 'L':
+                    current = ((current - distance) % size + size) % size;
+                    break;
+            }
+
+            if (current == 0)
+            {
+                zeros++;
+            }
+        }
+
+        return zeros;
+    }
+
+    /// <summary>
+    /// Calculates the number of times the pointer lands on zero after performing a series of rotations, taking into account multiple full rotations.
+    /// </summary>
+    public override object PartTwo(string input)
+    {
+        string[] rotations = Lines(input);
+
+        const int size = 100;
+        int current = 50;
+        int zeros = 0;
+        foreach (string rotation in rotations)
+        {
+            char direction = rotation[0];
+            int distance = int.Parse(rotation[1..]);
+
+            switch (direction)
+            {
+                case 'R':
+                    {
+                        int distToZero = size - current;
+                        if (distance >= distToZero)
+                        {
+                            zeros += 1 + (distance - distToZero) / size;
+                        }
+
                         current = (current + distance) % size;
                         break;
+                    }
+                case 'L':
+                    {
+                        int distToZero = current == 0 ? size : current;
 
-                    case 'L':
+                        if (distance >= distToZero)
+                        {
+                            zeros += 1 + (distance - distToZero) / size;
+                        }
+
                         current = ((current - distance) % size + size) % size;
                         break;
-                }
-
-                if (current == 0)
-                {
-                    zeros++;
-                }
+                    }
             }
-
-            return zeros;
         }
 
-        /// <summary>
-        /// Calculates the number of times the pointer lands on zero after performing a series of rotations, taking into account multiple full rotations.
-        /// </summary>
-        public override object PartTwo(string input)
-        {
-            string[] rotations = Lines(input);
-
-            const int size = 100;
-            int current = 50;
-            int zeros = 0;
-            foreach (string rotation in rotations)
-            {
-                char direction = rotation[0];
-                int distance = int.Parse(rotation[1..]);
-
-                switch (direction)
-                {
-                    case 'R':
-                        {
-                            int distToZero = size - current;
-                            if (distance >= distToZero)
-                            {
-                                zeros += 1 + (distance - distToZero) / size;
-                            }
-
-                            current = (current + distance) % size;
-                            break;
-                        }
-                    case 'L':
-                        {
-                            int distToZero = current == 0 ? size : current;
-
-                            if (distance >= distToZero)
-                            {
-                                zeros += 1 + (distance - distToZero) / size;
-                            }
-
-                            current = ((current - distance) % size + size) % size;
-                            break;
-                        }
-                }
-            }
-
-            return zeros;
-        }
+        return zeros;
     }
 }
--- src/AoC.Solutions/Y2025/Day03.cs
+++ src/AoC.Solutions/Y2025/Day03.cs
@@ -1,85 +1,84 @@
 ﻿using KE.AoC.Core.Solution;
 
-namespace KE.AoC.Solutions.Y2025
+namespace KE.AoC.Solutions.Y2025;
+
+[Solution(2025, 3)]
+public sealed class Day03 : SolutionBase
 {
-    [Solution(2025, 3)]
-    public sealed class Day03 : SolutionBase
+    /// <summary>
+    /// Calculates the sum of the maximum bank values that can be formed by selecting 2 digits from each bank in the input.
+    /// </summary>
+    public override object PartOne(string input)
     {
-        /// <summary>
-        /// Calculates the sum of the maximum bank values that can be formed by selecting 2 digits from each bank in the input.
-        /// </summary>
-        public override object PartOne(string input)
+        return Part(input, 2);
+    }
+
+    /// <summary>
+    /// Calculates the sum of the maximum bank values that can be formed by selecting 12 digits from each bank in the input.
+    /// </summary>
+    public override object PartTwo(string input)
+    {
+        return Part(input, 12);
+    }
+
+    /// <summary>
+    /// Calculates the sum of the maximum bank values that can be formed by selecting a specified number of digits from each bank in the input.
+    /// </summary>
+    private static object Part(string input, int digits)
+    {
+        string[] banks = Lines(input);
+
+        ulong result = 0;
+        foreach (string bank in banks)
         {
-            return Part(input, 2);
+            result += CalculateMaxBankValue(ParseBank(bank), digits);
         }
 
-        /// <summary>
-        /// Calculates the sum of the maximum bank values that can be formed by selecting 12 digits from each bank in the input.
-        /// </summary>
-        public override object PartTwo(string input)
-        {
-            return Part(input, 12);
-        }
+        return result;
+    }
 
-        /// <summary>
-        /// Calculates the sum of the maximum bank values that can be formed by selecting a specified number of digits from each bank in the input.
-        /// </summary>
-        private static object Part(string input, int digits)
-        {
-            string[] banks = Lines(input);
+    /// <summary>
+    /// Parses a string representation of a bank into an array of integers.
+    /// </summary>
+    private static int[] ParseBank(string bank)
+    {
+        return bank.Select(c => c - '0').ToArray();
+    }
 
-            ulong result = 0;
-            foreach (string bank in banks)
+    /// <summary>
+    /// Calculates the maximum bank value that can be formed by selecting a specified number of digits from the given array of battery values.
+    /// </summary>
+    private static ulong CalculateMaxBankValue(int[] batteries, int digits)
+    {
+        if (batteries.Length < digits)
+            return 0;
+
+        int[] values = new int[digits];
+
+        int startIdx = 0;
+        for (int i = 0; i < digits; i++)
+        {
+            int? max = null;
+            int? maxIdx = null;
+            for (int j = startIdx; j < batteries.Length - (digits - i) + 1; j++)
             {
-                result += CalculateMaxBankValue(ParseBank(bank), digits);
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Parses a string representation of a bank into an array of integers.
-        /// </summary>
-        private static int[] ParseBank(string bank)
-        {
-            return bank.Select(c => c - '0').ToArray();
-        }
-
-        /// <summary>
-        /// Calculates the maximum bank value that can be formed by selecting a specified number of digits from the given array of battery values.
-        /// </summary>
-        private static ulong CalculateMaxBankValue(int[] batteries, int digits)
-        {
-            if (batteries.Length < digits)
-                return 0;
-
-            int[] values = new int[digits];
-
-            int startIdx = 0;
-            for (int i = 0; i < digits; i++)
-            {
-                int? max = null;
-                int? maxIdx = null;
-                for (int j = startIdx; j < batteries.Length - (digits - i) + 1; j++)
+                int current = batteries[j];
+                if (max == null || current > max)
                 {
-                    int current = batteries[j];
-                    if (max == null || current > max)
-                    {
-                        max = current;
-                        maxIdx = j;
-                    }
+                    max = current;
+                    maxIdx = j;
                 }
-                values[i] = max!.Value;
-                startIdx = maxIdx!.Value + 1;
             }
-
-            ulong result = 0;
-            for (int i = 0; i < values.Length; i++)
-            {
-                result += (ulong)values[i] * (ulong)Math.Pow(10, values.Length - i - 1);
-            }
-
-            return result;
+            values[i] = max!.Value;
+            startIdx = maxIdx!.Value + 1;
         }
+
+        ulong result = 0;
+        for (int i = 0; i < values.Length; i++)
+        {
+            result += (ulong)values[i] * (ulong)Math.Pow(10, values.Length - i - 1);
+        }
+
+        return result;
     }
 }
