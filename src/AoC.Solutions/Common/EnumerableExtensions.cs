b4bbd8c2b9a653f677b850d8bb36170edd1c531c
Miroslav Rábek <rabek.miroslav@gmail.com>
2026-07-30 05:55:18 +0200

    Create an Enumerable.Trace method to debug LINQ queries.

---
+++ src/AoC.Solutions/Common/EnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+
+namespace KE.AoC.Solutions.Common
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> Trace<T>(this IEnumerable<T> source, string label, Func<T, string>? formatter = null)
+        {
+#if DEBUG
+            List<T> list = source.ToList();
+            string text = formatter is null
+                ? string.Join(", ", list)
+                : string.Join(", ", list.Select(formatter));
+            Debug.WriteLine($"{label} ({list.Count}): {text}");
+            return list;
+#else
+            return source;
+#endif
+        }
+    }
+}
