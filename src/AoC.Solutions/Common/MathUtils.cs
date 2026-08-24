55b198717b681b0d6dfc9eb4e0c79c2e87009d18
Miroslav Rábek <rabek.miroslav@gmail.com>
2026-08-24 14:42:01 +0200

    Add generic MathUtils.Divisors<T> method.

--- src/AoC.Solutions/Common/MathUtils.cs
+++ src/AoC.Solutions/Common/MathUtils.cs
@@ -27,4 +27,36 @@ public static class MathUtils
 
         return count;
     }
+
+    /// <summary>
+    /// Calculates the divisors of the given value.
+    /// </summary>
+    /// <typeparam name="T">The type of the value.</typeparam>
+    /// <param name="value">The value to calculate the divisors for.</param>
+    /// <returns>A list of divisors of the value.</returns>
+    public static List<T> Divisors<T>(T value) where T : IBinaryInteger<T>
+    {
+        List<T> divisors = [];
+
+        if (value == T.Zero)
+            return divisors;
+
+        value = T.Abs(value);
+
+        for (T i = T.One; i <= value / i; i++)
+        {
+            if (value % i == T.Zero)
+            {
+                divisors.Add(i);
+
+                T paired = value / i;
+                if (i != paired)
+                    divisors.Add(paired);
+            }
+        }
+
+        divisors.Sort();
+
+        return divisors;
+    }
 }
